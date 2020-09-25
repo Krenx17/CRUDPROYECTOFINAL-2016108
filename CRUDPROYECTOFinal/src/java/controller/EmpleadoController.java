d5a79f68f7e74a8846a520625506579e5a5aa733
cespana2016108 <cespana-2016108@kinal.edu.gt>
2020-09-24 19:05:26 -0600

    Se actualizo el proyecto

---
+++ CRUDPROYECTOFinal/src/java/controller/EmpleadoController.java
@@ -0,0 +1,109 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Empleado;
+import modelDAO.EmpleadoDAO;
+
+public class EmpleadoController extends HttpServlet {
+    
+    String listar = "view/listar.jsp";
+    String add = "view/add.jsp";
+    String edit= "view/edit.jsp";
+    Empleado nuevoEmpleado = new Empleado();
+    EmpleadoDAO nuevoEmpleadoDAO = new EmpleadoDAO();
+    int IdEmpleado;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Controlador</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String acceso = "";
+        String action = request.getParameter ("accion");
+        if (action.equalsIgnoreCase("listar")){
+            acceso = listar;
+        }else if(action.equalsIgnoreCase("add")){
+            acceso = add;
+        }else if(action.equalsIgnoreCase("Agregar")){
+            String DPI = request.getParameter("txtDPI");
+            String nombre = request.getParameter("txtNombre");
+            String Telefono = request.getParameter("txtTelefono");
+            String Estado = request.getParameter("txtEstado");
+            String User = request.getParameter("txtUser");
+            nuevoEmpleado.setDPI(DPI);
+            nuevoEmpleado.setNombres(nombre);
+            nuevoEmpleado.setNombres(Telefono);
+            nuevoEmpleado.setEstado(Estado);
+            nuevoEmpleado.setUser(User);
+
+            nuevoEmpleadoDAO.add(nuevoEmpleado);
+            acceso = listar;
+
+        }else if (action.equalsIgnoreCase("editar")){
+            request.setAttribute("IdEmp", request.getParameter("IdEmpleado"));
+            acceso= edit;
+        }else if (action.equalsIgnoreCase("Actualizar")){
+            IdEmpleado = Integer.parseInt(request.getParameter("txtIdEmpleado"));
+            String DPI = request.getParameter("txtDPI");
+            String nombres = request.getParameter("txtNombre");
+            String telefono = request.getParameter("txtTelefono");
+            String estado = request.getParameter("txtEstado");
+            String user = request.getParameter("txtUser");
+            nuevoEmpleado.setIdEmpleado(IdEmpleado);
+            nuevoEmpleado.setDPI(DPI);
+            nuevoEmpleado.setNombres(nombres);
+            nuevoEmpleado.setTelefono(telefono);
+            nuevoEmpleado.setEstado(estado);
+            nuevoEmpleado.setUser(user);
+            nuevoEmpleadoDAO.edit(nuevoEmpleado);
+            acceso= listar;
+        }else if (action.equalsIgnoreCase("eliminar")){
+            IdEmpleado = Integer.parseInt(request.getParameter("IdEmpleado"));
+            nuevoEmpleado.setIdEmpleado(IdEmpleado);
+            nuevoEmpleadoDAO.eliminar(IdEmpleado);
+            acceso= listar;
+        }
+        processRequest(request, response);
+    }
+
+    
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CRUDPROYECTOFinal/web/view/list.jsp
@@ -0,0 +1,56 @@
+<%@page import="java.util.Iterator"%>
+<%@page import="java.util.List"%>
+<%@page import="model.Empleado"%>
+<%@page import="modelDAO.EmpleadoDAO"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Listar Registros</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1> Datos en la entidad Empleado</h1>
+            <a> href="Controlador?accion=add">Agregar Registro <a/>
+                <br><br>
+            <table>
+                <thead>  
+                    <tr>
+                        <td> Codigo Empleado </td>
+                        <td>  <strong>DPI</strong></td>
+                        <td>  <strong>Nombres</strong></td>
+                        <td>  <strong>Telefono</strong></td>
+                        <td>  <strong>Estado</strong></td>
+                        <td>  <strong>User</strong></td>
+                        <td>  <strong>Acciones</strong></td>
+                    </tr
+                </thead
+                <%
+                            EmpleadoDAO dao = new EmpleadoDAO();
+                            List<Empleado> listarEmpleado = dao.listar();
+                            Iterator<Empleado> iterator= listarEmpleado.iterator();
+                            Empleado emp = null;
+                            while(iterator.hasNext()){
+                                emp = iterator.next();
+                            %>
+                <tbody>
+                    <tr>
+                        <td><%= emp.getIdEmpleado()%></td>
+                        <td><%= emp.getDPI()%></td>
+                        <td><%= emp.getNombres()%></td>
+                        <td><%= emp.getTelefono()%></td>
+                        <td><%= emp.getEstado()%></td>
+                        <td><%= emp.getUser()%></td>
+                        <td>
+                            <a>  href="Controlador?accion=editar&IdEmpleado=<%=emp.getIdEmpleado()%>">Editar</a> 
+                            <a> href="Controlador?accion=eliminar&IdEmpleado=<%=emp.getIdEmpleado()%>">Delete</a>
+
+                        </td>
+                    </tr>
+                    <%}%>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
